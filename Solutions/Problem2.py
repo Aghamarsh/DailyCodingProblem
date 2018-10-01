655270127fc27156494661252f8e597495ef1da5
Aghamarsh Varanasi <aghamarsh8@gmail.com>
2018-10-01 07:01:41 +0530

    Update Problem2.py

--- Solutions/Problem2.py
+++ Solutions/Problem2.py
@@ -11,10 +11,25 @@ l=list((map(int, input().split())))
 ans=[]
 product = 1
 
+zcount=0
+
 for i in range(0,len(l)):
+    if l[i] == 0 :
+        zcount = zcount + 1
+    else:
     product = product * l[i]
 
-for i in range(0,len(l)):
-    ans.append(divisor(product,l[i]))
-
+if zcount == 0:    
+    for i in range(0,len(l)):
+        ans.append(divisor(product,l[i]))
+elif zcount == 1 :
+     for i in range(0,len(l)):
+            if l[i] == 0:
+                ans.append(divisor(product,l[i]))
+            else :
+                ans.append(0)
+else:
+    for i in range(0,len(l)):
+          ans.append(0)
+            
 print(ans)
